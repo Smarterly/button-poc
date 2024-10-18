6be446f1caddc862cd356732722275719582bb03
ava-cushon <ava.dignam@cushon.co.uk>
2024-10-18 08:37:05 +0100

    separate files for themes

---
+++ packages/tokens-multiconfig/buildTokens.ts
@@ -0,0 +1,41 @@
+import StyleDictionary, { Config } from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+import metadata from './tokens/$metadata.json' assert { type: 'json' };
+
+register(StyleDictionary);
+
+const configs: Config[] = metadata.tokenSetOrder.map((fileName) => {
+  const destinationFileName = fileName.replace('alias/', '').replace('/', '-');
+  return {
+    log: {
+      verbosity: 'verbose',
+    },
+    source:
+      fileName === 'base'
+        ? ['tokens/base.json']
+        : ['tokens/base.json', `tokens/${fileName}.json`],
+    preprocessors: ['tokens-studio'],
+    buildPath: 'build/css/',
+    platforms: {
+      css: {
+        transformGroup: 'tokens-studio',
+        transforms: ['name/kebab'],
+        buildPath: 'assets/css/',
+        options: {
+          selector: `.${destinationFileName}`,
+        },
+        files: [
+          {
+            destination: `${destinationFileName}.css`,
+            format: 'css/variables',
+          },
+        ],
+      },
+    },
+  };
+});
+
+configs.forEach(async (config) => {
+  const sd = new StyleDictionary(config);
+  await sd.buildAllPlatforms();
+});
