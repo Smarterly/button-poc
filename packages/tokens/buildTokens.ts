6ca6008d74741437741a0d1c5e23f132feb6049d
ava-cushon <ava.dignam@cushon.co.uk>
2024-10-21 15:58:48 +0100

    use new token structure

--- packages/tokens/buildTokens.ts
+++ packages/tokens/buildTokens.ts
@@ -1,36 +1,54 @@
 import StyleDictionary, { Config } from 'style-dictionary';
-import { register } from '@tokens-studio/sd-transforms';
+import { expandTypesMap, register } from '@tokens-studio/sd-transforms';
 import metadata from './tokens/$metadata.json' assert { type: 'json' };
 
 register(StyleDictionary);
 
-const configs: Config[] = metadata.tokenSetOrder.map((fileName) => {
-  const destinationFileName = fileName.replace('alias/', '').replace('/', '-');
-  return {
-    log: {
-      verbosity: 'verbose',
-    },
-    source:
-      fileName === 'base'
-        ? ['tokens/base.json']
-        : ['tokens/base.json', `tokens/${fileName}.json`],
-    preprocessors: ['tokens-studio'],
-    buildPath: 'src/build/css/',
-    platforms: {
-      css: {
-        transformGroup: 'tokens-studio',
-        transforms: ['name/kebab'],
+const modes = metadata.tokenSetOrder
+  .filter((fileName) => fileName.includes('mapped/'))
+  .map((fileName) => fileName.replace('mapped/', ''));
+
+const configs: Config[] = metadata.tokenSetOrder
+  .filter((fileName) => fileName.includes('alias'))
+  .flatMap((fileName) => {
+    return modes.map((mode) => {
+      const destinationFileName = fileName
+        .replace('alias/', '')
+        .concat(`-${mode}`);
+      return {
+        log: {
+          verbosity: 'verbose',
+        },
+        expand: {
+          typesMap: expandTypesMap,
+        },
+        source:
+          fileName === 'base'
+            ? ['tokens/base.json']
+            : [
+                'tokens/base.json',
+                `tokens/${fileName}.json`,
+                `tokens/mapped/${mode}.json`,
+                'tokens/component.json',
+              ],
+        preprocessors: ['tokens-studio'],
         buildPath: 'src/build/css/',
-        files: [
-          {
-            destination: `${destinationFileName}.css`,
-            format: 'css/variables',
+        platforms: {
+          css: {
+            transformGroup: 'tokens-studio',
+            transforms: ['name/kebab'],
+            buildPath: 'src/build/css/',
+            files: [
+              {
+                destination: `${destinationFileName}.css`,
+                format: 'css/variables',
+              },
+            ],
           },
-        ],
-      },
-    },
-  };
-});
+        },
+      };
+    });
+  });
 
 configs.forEach(async (config) => {
   const sd = new StyleDictionary(config);
